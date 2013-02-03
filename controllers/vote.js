aec4454aadd2f924ebd00ae30dec390c150b76ab
Rohit Nair <rohitnair5@gmail.com>
2013-02-02 19:53:26 -0800

    removing user

--- controllers/vote.js
+++ controllers/vote.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose')
   , Vote = mongoose.model('Vote')
-  , User = mongoose.model('User')
   , Play = mongoose.model('Play')
 
 module.exports = function(app) {
@@ -9,23 +8,21 @@ module.exports = function(app) {
     console.log("POST: ");
     console.log(req.body);
     var phoneNumber = req.body.From;
-    User.findOne({ phoneNumber: phoneNumber }).exec(function(err, user) {
-      Play.find({}).sort('-_id').limit(1).exec(function(err, play) {
-        vote = new Vote({
-          value: req.body.Body,
-          _play: play[0]._id,
-          _user: user._id
-        });
-        vote.save(function (err) {
-          if (!err) {
-            return console.log("created");
-          } else {
-            return console.log(err);
-          }
-        });
-        return res.send(vote);
-      }); 
-    });
+    Play.find({}).sort('-_id').limit(1).exec(function(err, play) {
+      vote = new Vote({
+        value: req.body.Body,
+        phoneNumber: req.body.From,
+        _play: play[0]._id,
+      });
+      vote.save(function (err) {
+        if (!err) {
+          return console.log("created");
+        } else {
+          return console.log(err);
+        }
+      });
+      return res.send(vote);
+    }); 
   });
 
   app.get('/api/votes', function (req, res){
--- models/vote.js
+++ models/vote.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 var VoteSchema = new mongoose.Schema({
   value: String,
+  phoneNumber: String,
   _play: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Play' }],
-  _user: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 })
 
 
